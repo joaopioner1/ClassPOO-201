bdb5f5c058e1d1e3f5d36181b355b0177408fd6f
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-08-22 22:09:08 -0300

    Update Livro.java

--- src/entities_livro/Livro.java
+++ src/entities_livro/Livro.java
@@ -76,15 +76,3 @@ public class Livro {
 		this.numPagLidas = numPagLidas;
 	}
 }
-/*
-Projeto 1:
-Classe: Livro
-Atributos: titulo, páginas, número de páginas lidas.
-Método: recomeca –> que recomeça a leitura do livro
-Método: ler –> lê um número de páginas a partir de onde está e retorna se o livro
-já foi todo lido ou não (boolean). Observe que o número de páginas lidas não pode
-ultrapassar o número de páginas do livro.
-–> Faça testes criando um livro, realizando leituras e verificando se o livro acabou
-e em que página se encontra até o livro acabar, recomece a leitura e faça novas
-leituras.
-*/
